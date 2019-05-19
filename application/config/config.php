7c8e8beda8017b7d746b5742349767647a2629a9
Muhammad Hafiz Hasan <mhafizhasan@gmail.com>
2019-05-19 14:54:29 +0800

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://api-open-simulation.test';
+// $config['base_url'] = 'http://api-open-simulation.test';
+$config['base_url'] = 'https://api-open-simulation.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -27,4 +27,57 @@ class Api extends CI_Controller
 		echo json_encode($data);
 	}
 
+	public function userList()
+	{
+		echo '{
+						"data": [{
+							"id": 3,
+							"uuid": "ce075a11-bdca-40f1-b631-1165bc2c8835",
+							"cont_name": "AAAA",
+							"cont_nric": null,
+							"cont_passport": null,
+							"cont_mobile": "9999999",
+							"cont_email": "aaaa@urusewa.com",
+							"user_id": 1
+						}, {
+							"id": 9,
+							"uuid": "f8b8f1a7-3ca3-4891-a344-4c4921536b21",
+							"cont_name": "AAAAA",
+							"cont_nric": null,
+							"cont_passport": null,
+							"cont_mobile": "12345676",
+							"cont_email": "aaaaa@asd.com",
+							"user_id": 1
+						}, {
+							"id": 4,
+							"uuid": "684d9e45-d345-4fbd-8b7f-0c8f3d252a15",
+							"cont_name": "BBBBB",
+							"cont_nric": null,
+							"cont_passport": null,
+							"cont_mobile": "0123456789",
+							"cont_email": "hello@urusewa.com",
+							"user_id": 1
+						}, {
+							"id": 5,
+							"uuid": "4c64e47b-8592-4494-a30d-dd59cb4ec651",
+							"cont_name": "CCCCCC",
+							"cont_nric": null,
+							"cont_passport": null,
+							"cont_mobile": "99999",
+							"cont_email": "kajsda@ads.com",
+							"user_id": 1
+						}, {
+							"id": 6,
+							"uuid": "fa09e489-dceb-41aa-bf5f-9a5f985128d5",
+							"cont_name": "DDDDDD",
+							"cont_nric": null,
+							"cont_passport": null,
+							"cont_mobile": "qweqeq",
+							"cont_email": "asdad@asd.com",
+							"user_id": 1
+						}],
+						"total": 7
+					}';
+	}
+
 }
