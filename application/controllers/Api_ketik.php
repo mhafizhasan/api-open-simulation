91880320977aef9b9088fa5e1d3d0b5c8f73b0f7
Muhammad Hafiz Hasan <mhafizhasan@gmail.com>
2019-08-28 17:22:49 +0800

    attendance list x

--- application/controllers/Api_ketik.php
+++ application/controllers/Api_ketik.php
@@ -23,31 +23,46 @@ class Api_ketik extends CI_Controller
 		echo json_encode($data);
 	}
 
-	public function attendanceList()
+	public function attendanceList($x)
 	{
-		$data = [
-			'status' => 'ok',
-			'attendances' => [
-				[
-					'day' => 'Isnin',
-					'date' => '01-01-2020',
-					'clockIn' => '08:15',
-					'clockOut' => '17.20'
-				],
-				[
-					'day' => 'Selasa',
-					'date' => '02-01-2020',
-					'clockIn' => '08:15',
-					'clockOut' => '17.20'
-				],
-				[
-					'day' => 'Rabu',
-					'date' => '03-01-2020',
-					'clockIn' => '08:15',
-					'clockOut' => '17.20'
+		if($x == "1") {
+			$data = [
+				'status' => 'ok',
+				'attendances' => [
+					[
+						'day' => 'Isnin',
+						'date' => '01-01-2020',
+						'clockIn' => '08:15',
+						'clockOut' => '17.20'
+					],
 				]
-			]
-		];
+			];
+		} else {
+			$data = [
+				'status' => 'ok',
+				'attendances' => [
+					[
+						'day' => 'Isnin',
+						'date' => '01-01-2020',
+						'clockIn' => '08:15',
+						'clockOut' => '17.20'
+					],
+					[
+						'day' => 'Selasa',
+						'date' => '02-01-2020',
+						'clockIn' => '08:15',
+						'clockOut' => '17.20'
+					],
+					[
+						'day' => 'Rabu',
+						'date' => '03-01-2020',
+						'clockIn' => '08:15',
+						'clockOut' => '17.20'
+					]
+				]
+			];
+		}
+
 
 		echo json_encode($data);
 	}
